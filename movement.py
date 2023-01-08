2bbd6afe63a96397f2f9db673e0a776801f959f0
FaheemG22 <125843@shipley.ac.uk>
2023-01-08 17:46:53 +0000

    yes

---
+++ movement.py
@@ -0,0 +1,62 @@
+import pygame
+from options import *
+from threading import Thread
+import time
+import keyboard
+
+def player1():
+    # player 1
+
+        if (settings.keys[pygame.K_a] and p1.x >settings.radius) and (p1.x > p2.x + settings.radius * 2 or p1.y < p2.y - settings.radius or p1.y > p2.y + settings.radius or p1.x < p2.x+settings.radius):
+            p1.x -= settings.vel
+            p1.coordinate = [p1.x, p1.y]
+        if (settings.keys[pygame.K_d] and p1.x < settings.screen_width - settings.radius) and (p1.x < p2.x - settings.radius * 2 or p1.y > p2.y + settings.radius or p1.y < p2.y - settings.radius or p1.x > p2.x-settings.radius):
+            p1.x += settings.vel
+            p1.coordinate = [p1.x, p1.y]
+        if (settings.keys[pygame.K_w] and p1.y >settings.radius) and (p1.y > p2.y + settings.radius * 2 or p1.x < p2.x - settings.radius or p1.x > p2.x + settings.radius or p1.y < p2.y-settings.radius):
+            p1.y -= settings.vel
+            p1.coordinate = [p1.x, p1.y]
+        if (settings.keys[pygame.K_s] and p1.y < settings.screen_height - settings.radius) and (p1.y < p2.y - settings.radius * 2 or p1.x > p2.x + settings.radius or p1.x < p2.x - settings.radius or p1.y > p2.y+settings.radius):
+            p1.y += settings.vel
+            p1.coordinate = [p1.x, p1.y]
+
+
+def player2():
+    # player 2
+
+        if (settings.keys[pygame.K_LEFT] and p2.x >settings.radius) and (p2.x > p1.x + settings.radius * 2 or p2.y < p1.y - settings.radius or p2.y > p1.y + settings.radius or p2.x < p1.x+settings.radius):
+            p2.x -= settings.vel
+            p2.coordinate = [p2.x, p2.y]
+        if (settings.keys[pygame.K_RIGHT] and p2.x < settings.screen_width - settings.radius) and (p2.x < p1.x - settings.radius * 2 or p2.y > p1.y + settings.radius or p2.y < p1.y - settings.radius or p2.x > p1.x-settings.radius):
+            p2.x += settings.vel
+            p2.coordinate = [p2.x, p2.y]
+        if (settings.keys[pygame.K_UP] and p2.y >settings.radius) and (p2.y > p1.y + settings.radius * 2 or p2.x < p1.x - settings.radius or p2.x > p1.x + settings.radius or p2.y < p1.y-settings.radius):
+            p2.y -= settings.vel
+            p2.coordinate = [p2.x, p2.y]
+        if (settings.keys[pygame.K_DOWN] and p2.y < settings.screen_height - settings.radius) and (p2.y < p1.y - settings.radius * 2 or p2.x > p1.x + settings.radius or p2.x < p1.x - settings.radius or p2.y > p1.y+settings.radius):
+            p2.y += settings.vel
+            p2.coordinate = [p2.x, p2.y]
+
+
+def strings1():
+    while True:
+        if settings.keys[pygame.K_r]:
+            if p1.string:
+                p1.string = False
+                time.sleep(0.3)
+            else:
+                p1.string = True
+                time.sleep(0.3)
+
+def strings2():
+    while True:
+        if settings.keys[pygame.K_RSHIFT]:
+            if p2.string:
+                p2.string = False
+                time.sleep(0.3)
+            else:
+                p2.string = True
+                time.sleep(0.3)
+
+def controls():
+    player1(), player2()
---
+++ options.py
@@ -0,0 +1,27 @@
+class settings:
+    screen_width = 1920
+    screen_height = 1080
+
+    outline = (255, 255, 255)
+    background =(0, 0, 0)
+
+    radius = 40
+    vel = 8
+
+    keys = "a"
+
+class p1:
+    x = 0 + settings.radius
+    y = 0 + settings.radius
+    coordinate = [x, y]
+
+    colour = (255, 0, 0)
+    string = False
+class p2:
+    x = settings.screen_width - settings.radius
+    y = settings.screen_height - settings.radius
+    coordinate = [x, y]
+
+    colour = (0, 0, 255)
+    string = False
+
